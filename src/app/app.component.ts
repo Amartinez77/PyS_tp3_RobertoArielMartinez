5d515a881743eaecd5abd3535dbb568373241dce
Ariel Martinez <ariel.mtz85@gmail.com>
2025-04-27 19:44:27 -0300

    modificando estilos en punto1 y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { Punto1Component } from './layout/punto1/punto1.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/layout/punto1/punto1.component.ts
+++ src/app/layout/punto1/punto1.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+  //definicion inteface Noticia
+  interface Noticia {
+    id: number;
+    title: string;
+    description: string;
+    imageUrl: string;
+  }
+  
 @Component({
   selector: 'app-punto1',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './punto1.component.html',
   styleUrl: './punto1.component.css'
 })
-export class Punto1Component {
+  
+
+export class Punto1Component {  
+
+  noticias: Noticia[] = [
+    {
+      id: 1,
+      title: 'Una IA para detectar el cáncer',
+      description: 'La empresa china Alibaba tiene una IA para detectar el cáncer de páncreas. Es tan buena que EEUU ha acelerado su aprobación',
+      imageUrl: '/assets/images/noticia1.jpeg'
+    },
+    {
+      id: 2,
+      title: 'Momento Histórico para Samsung',
+      description: 'Samsung ha perdido un liderazgo de 30 años en la industria de los chips DRAM',
+      imageUrl: 'assets/images/noticia2.jpeg'
+    },
+    {
+      id: 3,
+      title: 'De 1 KB a 200 GB',
+      description: 'Lo que antes cabia en un disquete, hoy exige discos duros enteros. Los videojuegos no paran de crecer y su evolución guarda pistas sobre lo que se viene.',
+      imageUrl: '/assets/images/noticia3.jpg'
+    }
+  ];
 
 }
