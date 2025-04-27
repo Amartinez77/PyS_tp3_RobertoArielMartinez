07ade88349e6e271d5d475e45588e80f19ea8712
Ariel Martinez <ariel.mtz85@gmail.com>
2025-04-27 01:38:02 -0300

    agregando componente punto1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Punto1Component } from './layout/punto1/punto1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'punto1', component: Punto1Component },
+  { path: '', redirectTo: 'punto1', pathMatch: 'full' },
+  { path: '**', redirectTo: 'punto1' } // Redirige a 'punto1' si la ruta no coincide con ninguna definida
+
+  
+];
