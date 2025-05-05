cf50e4619e87b44aa8c4497b0a4bd165364d97e7
Ariel Martinez <ariel.mtz85@gmail.com>
2025-05-05 00:55:32 -0300

    agregando metodo eliminar a BoletoService. Agregando funcion actualizarResumen. Modificando html punto4

--- src/app/boleto.service.ts
+++ src/app/boleto.service.ts
@@ -6,18 +6,46 @@ import { Boleto } from './layout/punto4/punto4.component';
 })
 export class BoletoService {
 
+  private boletosKey: string = 'boletosVendidos'; //clave LocalStorage borrar para usar memoria
+
   // Array para almacenar los boletos
   private boletos: Boleto[] = [];
   
   constructor() { }
 
   // metodo para agregar un boleto al array de boletos
-  agregarBoleto(boleto: Boleto): void{
-    this.boletos.push(boleto);
+  //agregarBoleto(boleto: Boleto): void{
+  //  this.boletos.push(boleto);
+  //}
+
+  //metodo para agregar un boleto al LocalStorage
+  agregarBoleto(boleto: Boleto): void {
+    const boletos = this.obtenerBoletos(); // Obtener los boletos existentes
+    boletos.push(boleto); // Agregar el nuevo boleto al array
+    localStorage.setItem(this.boletosKey, JSON.stringify(boletos)); // Guardar en LocalStorage
   }
 
   // metodo para obtener todos los boletos
-  obtenerBoletos(): Boleto[] { 
+  /* obtenerBoletos(): Boleto[] {
     return this.boletos;
+  } */
+  
+  // metodo para obtener todos los boletos del LocalStorage
+  obtenerBoletos(): Boleto[] {
+    const boletos = localStorage.getItem(this.boletosKey); // Obtener los boletos del LocalStorage
+    return boletos ? JSON.parse(boletos) : []; // Parsear y devolver el array de boletos o un array vacío si no hay boletos
+  }
+
+  // metodo para eliminar un boleto por su dni
+  /* eliminarBoleto(dni: string): void {
+    this.boletos = this.boletos.filter(boleto => boleto.dni !== dni)
+  } */
+
+  // metodo para eliminar un boleto por su dni del LocalStorage
+  eliminarBoleto(dni: string): void {
+    let boletos = this.obtenerBoletos(); // Obtener los boletos existentes
+    boletos = boletos.filter(boleto => boleto.dni !== dni); // Filtrar los boletos que no coinciden con el dni
+    localStorage.setItem(this.boletosKey, JSON.stringify(boletos)); // Guardar el nuevo array en LocalStorage
+
   }
 }
--- src/app/layout/punto4/punto4.component.ts
+++ src/app/layout/punto4/punto4.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
 import { BoletoService } from '../../boleto.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { DataTablesModule } from 'angular-datatables';
+import { Subject } from 'rxjs';
 
 //interface para el objeto boleto
 export interface Boleto{
@@ -15,13 +17,19 @@ export interface Boleto{
 
 @Component({
   selector: 'app-punto4',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, DataTablesModule],
   templateUrl: './punto4.component.html',
   styleUrl: './punto4.component.css'
 })
-export class Punto4Component {
+export class Punto4Component implements OnInit, OnDestroy {
+
+  boletosVendidos: Boleto[] = []; // Array para almacenar los boletos vendidos
+
+  // para el datatable
+  dtOptions: any = {};
+  dtTrigger: Subject<any> = new Subject<any>();
   
-  constructor(boletoService: BoletoService) { } // Constructor de la clase
+  constructor(private boletoService: BoletoService) { } // Constructor de la clase
   
   precioBase: number = 10000;
   precioConDescuento: number = 0;
@@ -51,7 +59,99 @@ export class Punto4Component {
 
   //funcion para actualer el precio del boleto
   actualizarPrecio(): void {
-    this.boleto.precio = this.precioTotal(this.boleto.categoriaTurista);
+    this.precioConDescuento = this.precioTotal(this.boleto.categoriaTurista);
+  }
+
+  // funcion para agregar el boleto al array de boletos
+  agregarBoleto(): void {
+    this.boleto.precio = this.precioFinal; // Asignar el precio final al boleto
+    this.boletoService.agregarBoleto(this.boleto);
+
+    this.boletosVendidos = this.boletoService.obtenerBoletos(); // Actualizar el array de boletos vendidos
+    this.actualizarResumen(); // Actualizar el resumen de ventas
+
+    this.boleto = {
+      dni: '',
+      nombre: '',
+      categoriaTurista: 0,
+      fechaCompra: new Date(),
+      email: '',
+      precio: 0
+    };
+  }
+
+  // funcion que devuelve al html el precio con descuento
+
+  get precioFinal(): number { 
+    if (!this.boleto.categoriaTurista || !this.precioBase) return 0;
+
+    console.log(this.boleto.categoriaTurista, this.precioBase);
+
+    switch (this.boleto.categoriaTurista) {
+      case 1: return this.precioBase * 0.65; // 35% de descuento
+      case 2: return this.precioBase; // sin descuento
+      case 3: return this.precioBase * 0.5; // 50% de descuento
+      default: return this.precioBase; // por si no coincide ninguna categoría válida
+    }
+  }
+
+
+
+  // funcion para inicializar el datatable
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json'
+      }      
+    };
+
+    this.actualizarResumen() // Actualiza el resumen de ventas al iniciar el componente
+    this.boletosVendidos = this.boletoService.obtenerBoletos();
+    this.dtTrigger.next(null); // inicia el DataTable
+  }
+  
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
+
+  // array de categorias de turistas
+  categorias = [
+    { id: 0, nombre: 'Todos' },
+    { id: 1, nombre: 'Menor' },
+    { id: 2, nombre: 'Adulto' },
+    { id: 3, nombre: 'Jubilado' },
+  ];
+
+  categoriaSeleccionada: number = 0; // variable para almacenar la categoria seleccionada por el usuario
+
+
+  resumenTotalVentas: number = 0;
+  resumenTotalRecaudado: number = 0;
+  
+  actualizarResumen(): void {
+    console.log(this.categoriaSeleccionada);
+    const boletos = this.boletoService.obtenerBoletos();
+    console.log(boletos);
+    console.log('Tipo de categoría seleccionada:', typeof this.categoriaSeleccionada);
+
+
+    const filtrados = this.categoriaSeleccionada === 0
+      ? boletos
+      : boletos.filter(b => b.categoriaTurista === this.categoriaSeleccionada);
+
+    this.resumenTotalVentas = filtrados.length;
+    this.resumenTotalRecaudado = filtrados.reduce((suma, b) => suma + b.precio, 0);
+}
+
+  // funcion para eliminar un boleto por su dni
+  eliminarBoleto(dni: string): void{
+    this.boletoService.eliminarBoleto(dni); // eliminar el boleto del array de boletos
+    this.boletosVendidos = this.boletoService.obtenerBoletos(); // actualizar el array de boletos vendidos
+    this.actualizarResumen(); // actualizar el resumen de ventas  
+
   }
 
 }
